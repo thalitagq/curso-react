dca4bacb16d98be83bb8d0d96d2b9bcb95d33b42
thalita <thalitagq@outlook.com>
2021-06-07 15:54:47 -0300

    projeto 4

---
+++ projeto-4/src/context/auth-context.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+
+const AuthContext = React.createContext({
+  isLoogedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
+});
+
+export function AuthContextProvider(props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthContext;
