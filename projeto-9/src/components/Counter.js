d99fc67cd1e53e306375718061a0b572c54f7fee
thalita <thalitagq@outlook.com>
2021-06-14 13:14:35 -0300

    feat: :sparkles: use redux to manage state

---
+++ projeto-9/src/components/Counter.js
@@ -0,0 +1,40 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { counterActions } from '../store/counter';
+import classes from './Counter.module.css';
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter.counter);
+  const show = useSelector(state => state.counter.showCounter);
+
+  const incrementHandler  = () => {
+    dispatch(counterActions.increment());
+  }
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  }
+
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(5));
+  }
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
