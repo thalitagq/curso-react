8561b9182755de39beb354de2ac9da6f67823987
thalita <thalitagq@outlook.com>
2021-06-15 12:05:21 -0300

    feat: :sparkles: introduce the react router

---
+++ projeto-11/src/components/MainHeader.js
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+import styles from './MainHeader.module.css';
+
+function MainHeader() {
+  return (
+    <header className={styles.header}>
+      <ul>
+        <li>
+          <NavLink activeClassName={styles.active} to="/welcome">
+            Welcome
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName={styles.active} to="/products">
+            Products
+          </NavLink>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default MainHeader;
