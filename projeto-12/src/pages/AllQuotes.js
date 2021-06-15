5ea6f5a1861181ef411ab46c86bdb9dc70051377
thalita <thalitagq@outlook.com>
2021-06-15 17:52:45 -0300

    feat: :sparkles: add new project witch works with react router and http requests

---
+++ projeto-12/src/pages/AllQuotes.js
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import QuoteList from "../components/quotes/QuoteList";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from '../lib/api';
+import LoadinSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
+
+
+function AllQuotes() {
+  const {sendRequest, status, data: loadedQuotes, error } = useHttp(getAllQuotes, true);
+  
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest])
+
+  if (status === 'pending') {
+    return(
+      <div className='centered'>
+        <LoadinSpinner/>
+      </div>
+    )
+  }
+
+  if (error) {
+    return <p className='centered focused'>{error}</p>
+  }
+
+  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound/>
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
+}
+
+export default AllQuotes;
