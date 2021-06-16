f24372b62a594051067f9a3a5d062970060ab776
thalita <thalitagq@outlook.com>
2021-06-16 16:27:55 -0300

    feat: :sparkles: add demo project witch uses authentication via firebase authentication api

---
+++ projeto-13/src/components/Layout/MainNavigation.js
@@ -0,0 +1,37 @@
+import { useContext, Fragment } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../../store/auth-context";
+
+import classes from "./MainNavigation.module.css";
+
+const MainNavigation = () => {
+  const { isLoggedIn, logout } = useContext(AuthContext);
+  return (
+    <header className={classes.header}>
+      <Link to="/">
+        <div className={classes.logo}>React Auth</div>
+      </Link>
+      <nav>
+        <ul>
+          {!isLoggedIn && (
+            <li>
+              <Link to="/auth">Login</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <Fragment>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <button onClick={logout}>Logout</button>
+              </li>
+            </Fragment>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
